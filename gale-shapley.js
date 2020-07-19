0cb860cba016364735525f94f6aa3ce1a21c7908
Andy Wang <andy_wang@comcast.net>
2020-07-18 20:11:17 -0700

    working gale-shapley

---
+++ gale-shapley.js
@@ -0,0 +1,72 @@
+//input will be two objects - male and female preference tables
+//keys will be individual names and values will be rank-ordered preference arrays
+
+function stableMarriage(malePref, femalePref) {
+  if (Object.keys(malePref).length !== Object.keys(femalePref).length)
+    return Error;
+
+  //create proposal table to store temp engagements
+  const proposalTable = {};
+  for (const female in femalePref) proposalTable[female] = null;
+
+  while (Object.values(proposalTable).includes(null)) {
+    //each male proposes to female he prefers most
+    //each female is temp engaged to most preferred suitor and rejects all other proposals
+    for (const male in malePref) {
+      const maleTopPref = malePref[male][0];
+      const targetFemalePrefList = femalePref[maleTopPref];
+
+      //if female has no temp proposal, then create one
+      if (!proposalTable[maleTopPref]) proposalTable[maleTopPref] = male;
+      //else evaluate whether or not she prefers current proposal
+      //compare index positions of current proposal to current engagement
+      else if (
+        targetFemalePrefList.indexOf(male) <
+        targetFemalePrefList.indexOf(proposalTable[maleTopPref])
+      ) {
+        const replacedMale = proposalTable[maleTopPref];
+        //male who will be replaced will need to adjust his pref list
+        malePref[replacedMale] = malePref[replacedMale].slice(1);
+        proposalTable[maleTopPref] = male;
+      } else if (
+        targetFemalePrefList.indexOf(male) >
+        targetFemalePrefList.indexOf(proposalTable[maleTopPref])
+      ) {
+        //if proposer got rejected, he will adjust his pref list
+        malePref[male] = malePref[male].slice(1);
+      }
+    }
+  }
+
+  return proposalTable;
+}
+
+const testMales = {
+  m1: ['w1', 'w2', 'w3', 'w4'],
+  m2: ['w1', 'w4', 'w3', 'w2'],
+  m3: ['w2', 'w1', 'w3', 'w4'],
+  m4: ['w4', 'w2', 'w3', 'w1'],
+};
+
+const testFemales = {
+  w1: ['m4', 'm3', 'm1', 'm2'],
+  w2: ['m2', 'm4', 'm1', 'm3'],
+  w3: ['m4', 'm1', 'm2', 'm3'],
+  w4: ['m3', 'm2', 'm1', 'm4'],
+};
+
+console.log(stableMarriage(testMales, testFemales));
+
+const testMales2 = {
+  a: ['y', 'x', 'z'],
+  b: ['z', 'y', 'x'],
+  c: ['x', 'z', 'y'],
+};
+
+const testFemales2 = {
+  x: ['b', 'a', 'c'],
+  y: ['c', 'b', 'a'],
+  z: ['a', 'c', 'b'],
+};
+
+console.log(stableMarriage(testMales2, testFemales2));
---
+++ irving.js
@@ -0,0 +1,99 @@
+//input will be one object
+//keys will be individual names and values will be rank-ordered preference arrays
+
+function stableRoomies(prefTable) {
+  //if stableTable is falsy, then we failed to make stable matches in first phase
+  //prompt user with option to randomly swap one set of preferences
+  const stableTable = firstPhase(prefTable);
+  if (!stableTable) return { error: 'Unstable Matches @ phase 1' };
+  const reducedTable = reduceStableTables(stableTable, prefTable);
+
+  console.log(reducedTable);
+}
+
+//pretty much same logic as gale-shapley algo
+function firstPhase(table) {
+  let inputTable = { ...table };
+
+  const proposalTable = {};
+  //if someone is rejected by all others, then it's impossible to have stable matches
+  //given the current preference lists
+  let impossibleMatchCondition = false;
+  for (const person in inputTable) proposalTable[person] = null;
+
+  while (
+    Object.values(proposalTable).includes(null) &&
+    !impossibleMatchCondition
+  ) {
+    for (const person in inputTable) {
+      const personsTopPref = inputTable[person][0];
+      const targetPrefList = inputTable[personsTopPref];
+
+      if (!proposalTable[personsTopPref])
+        proposalTable[personsTopPref] = person;
+      else if (
+        targetPrefList.indexOf(person) <
+        targetPrefList.indexOf(proposalTable[personsTopPref])
+      ) {
+        const replacedPerson = proposalTable[personsTopPref];
+        inputTable[replacedPerson] = inputTable[replacedPerson].slice(1);
+        proposalTable[personsTopPref] = person;
+        if (!inputTable[replacedPerson].length) impossibleMatchCondition = true;
+      } else if (
+        targetPrefList.indexOf(person) >
+        targetPrefList.indexOf(proposalTable[personsTopPref])
+      ) {
+        inputTable[person] = inputTable[person].slice(1);
+        if (!inputTable[person].length) impossibleMatchCondition = true;
+      }
+    }
+  }
+  return impossibleMatchCondition ? false : proposalTable;
+}
+
+function reduceStableTables(proposalTable, preferenceTable) {
+  let prefTable = { ...preferenceTable };
+
+  //based on proposal table: for each person, we will adjust
+  //their preference table such that every other person
+  //lower ranked than their current tentative partner will be removed;
+  //deleted person will then remove whoever just removed them from their list
+
+  for (const person in proposalTable) {
+    const currPartner = proposalTable[person];
+
+    const removedArr = prefTable[person].splice(
+      prefTable[person].indexOf(currPartner) + 1
+    );
+
+    removedArr.forEach((removedPerson) => {
+      prefTable[removedPerson].splice(
+        prefTable[removedPerson].indexOf(person),
+        1
+      );
+    });
+  }
+  return prefTable;
+}
+
+// function phaseTwo()
+
+const brokenInput = {
+  a: ['b', 'c', 'd'],
+  b: ['c', 'a', 'd'],
+  c: ['a', 'b', 'd'],
+  d: ['a', 'b', 'c'],
+};
+
+// console.log(firstPhase(brokenInput));
+
+const validInput = {
+  1: ['4', '6', '2', '5', '3'],
+  2: ['6', '3', '5', '1', '4'],
+  3: ['4', '5', '1', '6', '2'],
+  4: ['2', '6', '5', '1', '3'],
+  5: ['4', '2', '3', '6', '1'],
+  6: ['5', '1', '4', '2', '3'],
+};
+
+console.log(stableRoomies(validInput));
