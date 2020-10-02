9dfaf22c11fd66f0ad7da4860e264c3d8c23cd2b
Andy Wang <andy_wang@comcast.net>
2020-10-02 14:54:48 -0700

    feat(test): stable marriage testing complete

--- __tests__/algo.test.js
+++ __tests__/algo.test.js
@@ -1,4 +1,5 @@
 import stableRoomies from '../algos/irving';
+import stableMarriage from '../algos/gale-shapley';
 
 //Integration Testing for Irving Algo
 describe('Irving/Stable Roomie Algorithm', () => {
@@ -105,4 +106,43 @@ describe('Irving/Stable Roomie Algorithm', () => {
       Tammy: ['Ginny'],
     });
   });
+});
+
+describe('Stable Marriage/Gale-Shapley', () => {
+  it('should correctly derive pairings', () => {
+    const testMales = {
+      m1: ['w1', 'w2', 'w3', 'w4'],
+      m2: ['w1', 'w4', 'w3', 'w2'],
+      m3: ['w2', 'w1', 'w3', 'w4'],
+      m4: ['w4', 'w2', 'w3', 'w1'],
+    };
+
+    const testFemales = {
+      w1: ['m4', 'm3', 'm1', 'm2'],
+      w2: ['m2', 'm4', 'm1', 'm3'],
+      w3: ['m4', 'm1', 'm2', 'm3'],
+      w4: ['m3', 'm2', 'm1', 'm4'],
+    };
+
+    expect(stableMarriage(testMales, testFemales)).toEqual({
+      w1: 'm3',
+      w2: 'm4',
+      w3: 'm1',
+      w4: 'm2',
+    });
+  });
+
+  const testMales2 = {
+    a: ['y', 'x', 'z'],
+    b: ['z', 'y', 'x'],
+    c: ['x', 'z', 'y'],
+  };
+
+  const testFemales2 = {
+    x: ['b', 'a', 'c'],
+    y: ['c', 'b', 'a'],
+    z: ['a', 'c', 'b'],
+  };
+
+  expect(stableMarriage(testMales2, testFemales2)).toEqual({ x: 'c', y: 'a', z: 'b' });
 });
--- algos/gale-shapley.js
+++ algos/gale-shapley.js
@@ -7,8 +7,7 @@ function stableMarriage(malePreference, femalePreference) {
   const malePref = cloneDeep(malePreference);
   const femalePref = cloneDeep(femalePreference);
 
-  if (Object.keys(malePref).length !== Object.keys(femalePref).length)
-    return Error;
+  if (Object.keys(malePref).length !== Object.keys(femalePref).length) return Error;
 
   //create proposal table to store temp engagements
   const proposalTable = {};
@@ -45,35 +44,5 @@ function stableMarriage(malePreference, femalePreference) {
 
   return proposalTable;
 }
-
-//-----------TESTS-------------
-const testMales = {
-  m1: ['w1', 'w2', 'w3', 'w4'],
-  m2: ['w1', 'w4', 'w3', 'w2'],
-  m3: ['w2', 'w1', 'w3', 'w4'],
-  m4: ['w4', 'w2', 'w3', 'w1'],
-};
-
-const testFemales = {
-  w1: ['m4', 'm3', 'm1', 'm2'],
-  w2: ['m2', 'm4', 'm1', 'm3'],
-  w3: ['m4', 'm1', 'm2', 'm3'],
-  w4: ['m3', 'm2', 'm1', 'm4'],
-};
-// console.log(stableMarriage(testMales, testFemales));
-
-const testMales2 = {
-  a: ['y', 'x', 'z'],
-  b: ['z', 'y', 'x'],
-  c: ['x', 'z', 'y'],
-};
-
-const testFemales2 = {
-  x: ['b', 'a', 'c'],
-  y: ['c', 'b', 'a'],
-  z: ['a', 'c', 'b'],
-};
-
-// console.log(stableMarriage(testMales2, testFemales2));
 
 module.exports = stableMarriage;
